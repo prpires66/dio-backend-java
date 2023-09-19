2347c90241a2d2bec8b16e7dcecf06494bd37cf0
Paulo Pires <prpires66@gmail.com>
2023-09-19 17:14:38 -0300

    Anatomia das Classes - 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,13 @@
 public class MinhaClasse {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    String primeiroNome = "Paulo";
+    String segundoNome = "Pires";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+  }
+
+  public static String nomeCompleto(String primeiroNome, String segundoNome) {
+    return primeiroNome.concat(" ").concat(segundoNome);
   }
 }
