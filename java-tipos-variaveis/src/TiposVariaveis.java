3ba3a232db1ca60c50c33591485fa4811bd88578
Paulo Pires <prpires66@gmail.com>
2023-09-19 22:54:11 -0300

    Tipos e Variáveis - 4

--- java-tipos-variaveis/src/TiposVariaveis.java
+++ java-tipos-variaveis/src/TiposVariaveis.java
@@ -4,5 +4,9 @@ public class TiposVariaveis {
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
     }
 }
