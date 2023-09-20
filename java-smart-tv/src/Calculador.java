26a7e9c5730e28515a6b61ab1f4e058ba26964cf
Paulo Pires <prpires66@gmail.com>
2023-09-20 16:55:36 -0300

    Java Doc

---
+++ java-smart-tv/src/Calculador.java
@@ -0,0 +1,27 @@
+/**
+ * <h1>Calculador</h1>
+ * <p>
+ * A Calculdora realiza operações matemáticas entre números inteiros
+ * </p>
+ * <b>Note:</b> Leia atentamente a documentação desta classe
+ * para desfrutar dos recursos oferecidos pelo autor
+ * 
+ * @author Paulo Pires
+ * @version 1.0
+ * @since 01/01/2022
+ */
+public class Calculador {
+  /**
+   * Este método é utilizado para somar dois números inteiros.
+   * 
+   * @param numeroUm   este é o primeiro parâmetro do método.
+   * 
+   * @param numeroDois este é o segundo parâmetro do método.
+   * 
+   * @return int o resultado deste método é a soma dos dois numeros.
+   */
+
+  public int somar(int numeroUm, int numeroDois) {
+    return numeroUm + numeroDois;
+  }
+}
---
+++ java-smart-tv/src/Comentarios.java
@@ -0,0 +1,19 @@
+public class Comentarios {
+  public static void main(String[] args) {
+    // Olá, eu sou um comentário em uma única linha
+
+    /*
+     * Olá, eu sou um comentário
+     * que pode ser mais detalhado
+     * quando necessário
+     */
+
+    /**
+     * Estas duas estrelinha acima
+     * é para identificar que você
+     * pretende elaborar um comentário
+     * a nível de documentação
+     * Qua incrível!!!
+     */
+  }
+}
