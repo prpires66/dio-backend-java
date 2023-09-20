77b55ee60eb1a8801c41a89454ddde51d5d4e6c7
Paulo Pires <prpires66@gmail.com>
2023-09-20 12:17:18 -0300

    Métodos - 4

--- java-smart-tv/src/Usuario.java
+++ java-smart-tv/src/Usuario.java
@@ -5,5 +5,16 @@ public class Usuario {
         System.out.println("TV Ligada? " + smartTv.ligada);
         System.out.println("Canal Atual ? " + smartTv.canal);
         System.out.println("Volume Atual? " + smartTv.volume);
+
+        smartTv.ligar();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarCanal();
+        System.out.println("TV Ligada? " + smartTv.ligada);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+        System.out.println("Volume Atual? " + smartTv.volume);
+        smartTv.mudarCanal(13);
+        System.out.println("Canal Atual ? " + smartTv.canal);
+
     }
 }
