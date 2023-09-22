ffbf9c7d3b55d5322dc7bbb3ab786cbca1e1fbdc
Paulo Pires <prpires66@gmail.com>
2023-09-22 14:46:44 -0300

    Estruturas de repetição - For

---
+++ java-controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+  public static void main(String[] args) {
+    System.out.println("Contando carneirinhos");
+    for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+      System.out.println("Carneirinho " + carneirinhos);
+    }
+    System.out.println("Joãozinho Dormiu");
+  }
+}
