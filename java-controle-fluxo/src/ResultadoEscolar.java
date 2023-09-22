64d5ec10b526d321ecf05ac14f63514a198c8d57
Paulo Pires <prpires66@gmail.com>
2023-09-22 12:54:51 -0300

    Condição ternária 1

--- java-controle-fluxo/src/ResultadoEscolar.java
+++ java-controle-fluxo/src/ResultadoEscolar.java
@@ -1,12 +1,7 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
     int nota = 7;
-
-    if (nota >= 7)
-      System.out.println("Aprovado");
-    else if (nota >= 5 && nota < 7)
-      System.out.println("Prova Recuperação");
-    else
-      System.out.println("Reprovado");
+    String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+    System.out.println(resultado);
   }
 }
