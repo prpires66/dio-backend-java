47f43e04a131a98edff848d647494a3ede258746
Paulo Pires <prpires66@gmail.com>
2023-09-22 12:49:49 -0300

    Condicional encadeada

--- java-controle-fluxo/src/ResultadoEscolar.java
+++ java-controle-fluxo/src/ResultadoEscolar.java
@@ -1,9 +1,11 @@
 public class ResultadoEscolar {
   public static void main(String[] args) {
-    int nota = 6;
+    int nota = 7;
 
-    if (nota <= 7)
+    if (nota >= 7)
       System.out.println("Aprovado");
+    else if (nota >= 5 && nota < 7)
+      System.out.println("Prova Recuperação");
     else
       System.out.println("Reprovado");
   }
